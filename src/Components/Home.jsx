2ae93b5d4618228542d2ef0bb91cff843ebca1f1
Zeeshu1708 <zeeshanahmed1708@gmail.com>
2023-08-21 02:46:47 +0530

    project completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import './Home.scss'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { BiPlay } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const apikey = "76de5365fba15aa770b0eabd6fdac67b";
 const url = "https://api.themoviedb.org/3"
@@ -39,6 +42,7 @@ const Home = () => {
     const fetchupcoming = async() => {
        const {data:{results}} = await axios.get(`${url}/movie/${upcoming}?api_key=${apikey}`);
        setUpcomingMovies(results);
+       console.log(results);
     };
     const fetchpopular = async() => {
        const {data:{results}} = await axios.get(`${url}/movie/${nowPlaying}?api_key=${apikey}`);
@@ -57,7 +61,7 @@ const Home = () => {
           data: { genres },
       } = await axios.get(`${url}/genre/movie/list?api_key=${apikey}`);
       setGenre(genres);
-      console.log(genres);
+      // console.log(genres);
   };
 
 
@@ -74,11 +78,31 @@ const Home = () => {
 
   return (
     <section className="home">
-      <div className="banner"></div>
+          {/* <div className="banner" style={{backgroundImage: popularMovies[3]
+                        ? `url(${`${imgUrl}/${popularMovies[3].poster_path}`})`
+                        : "rgb(16, 16, 16)",
+                }}
+            > */}
+          <div className="banner" >
+          <h1>Oppenheimer</h1>
+          <p>The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.</p>
+          <div >
+          <button>Play  <BiPlay/> </button>
+          <button>My List <AiOutlinePlus/> </button>
+          </div>
+          </div>
       <Row title={"Upcoming Movies"} arr={upcomingMovies}/>
       <Row title={"Available Movies"} arr={nowPlayingMovies}/>
       <Row title={"Popular Movies"} arr={popularMovies}/>
       <Row title={"Top Rated Movies"} arr={topRatedMovies}/>
+
+      <div className="genreBox">
+        {
+          genre.map((item)=>(
+            <Link key={item.id} to={`/genre/${item.id}`}>{item.name}</Link>
+          ))
+        }
+      </div>
     </section>
   )
 }
